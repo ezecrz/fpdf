568fcb5400d5695e26f41da228005c4f110adaf9
ezecrz <otro.ezequielcruz@gmail.com>
2024-11-24 16:24:53 -0600

    Update fpdf.php

--- fpdf.php
+++ fpdf.php
@@ -7,7 +7,7 @@
 * Author:  Olivier PLATHEY                                                     *
 *******************************************************************************/
 
-define'FPDF_VERSION','1.81';
+define('FPDF_VERSION','1.81');
 
 class FPDF
 {
